b504549cec3a931401a67c98d05c4d460785e2e0
Lin_Andy <221502@astanait.edu.kz>
2024-04-17 22:22:50 +0600

    commit

--- src/merge.rs
+++ src/merge.rs
@@ -3,34 +3,27 @@ pub fn merge_sort<T: Ord + Clone>(arr: &mut [T]) {
         return;
     }
     let mid = arr.len() / 2;
-    let (left, right) = arr.split_at_mut(mid);
+    let (left, right) = arr.split_at(mid);
+    let mut merged = Vec::with_capacity(arr.len());
     merge_sort(left);
     merge_sort(right);
-    merge(left, right, arr);
+    merge(left, right, &mut merged);
+    arr.copy_from_slice(&merged);
 }
 
-fn merge<T: Ord + Clone>(left: &[T], right: &[T], arr: &mut [T]) {
+fn merge<T: Ord + Clone>(left: &[T], right: &[T], merged: &mut Vec<T>) {
     let mut left_index = 0;
     let mut right_index = 0;
-    let mut arr_index = 0;
     while left_index < left.len() && right_index < right.len() {
         if left[left_index] <= right[right_index] {
-            arr[arr_index] = left[left_index].clone();
+            merged.push(left[left_index].clone());
             left_index += 1;
         } else {
-            arr[arr_index] = right[right_index].clone();
+            merged.push(right[right_index].clone());
             right_index += 1;
         }
-        arr_index += 1;
     }
-    while left_index < left.len() {
-        arr[arr_index] = left[left_index].clone();
-        left_index += 1;
-        arr_index += 1;
-    }
-    while right_index < right.len() {
-        arr[arr_index] = right[right_index].clone();
-        right_index += 1;
-        arr_index += 1;
-    }
+    // Copy remaining elements from left and right if any
+    merged.extend_from_slice(&left[left_index..]);
+    merged.extend_from_slice(&right[right_index..]);
 }
