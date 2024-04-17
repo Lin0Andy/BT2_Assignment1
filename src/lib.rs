d1b70b194cbca97871b10496ba36dfc442e23cea
Lin_Andy <221502@astanait.edu.kz>
2024-04-17 22:21:18 +0600

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 
-mod quick_sort;
-mod selection_sort;
-mod insertion_sort;
-mod merge_sort;
+mod quick;
+mod selection;
+mod insertion;
+mod merge;
 
 
-pub use quick_sort::quick_sort;
-pub use selection_sort::selection_sort;
-pub use insertion_sort::insertion_sort;
-pub use merge_sort::merge_sort;
+pub use quick::quick_sort;
+pub use selection::selection_sort;
+pub use insertion::insertion_sort;
+pub use merge::merge_sort;
